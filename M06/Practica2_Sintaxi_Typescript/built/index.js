2744604eeed860c6bbd1fdedc1d110ec2e22b150
varmir1 <neilvargascalle@elpuig.xeill.net>
2024-10-21 20:55:26 +0200

    built: ex 3 working

--- M06/Practica2_Sintaxi_Typescript/built/index.js
+++ M06/Practica2_Sintaxi_Typescript/built/index.js
@@ -1,3 +1,30 @@
 "use strict";
-var pepe = 1;
-console.log(pepe);
+const visitas_maximas = 3;
+let nombre_pagina;
+let contador_vis = 2;
+var mensaje_bv = "Hola, bienvenido!";
+do {
+    if (contador_vis < visitas_maximas) {
+        contador_vis++;
+        console.log;
+    }
+    else if (contador_vis = visitas_maximas) {
+        contador_vis++;
+    }
+} while (contador_vis < visitas_maximas);
+if (contador_vis < visitas_maximas) {
+    contador_vis++;
+    console.log(mensaje_bv);
+    if (contador_vis <= visitas_maximas) {
+        contador_vis++;
+        console.log(mensaje_bv);
+        if (contador_vis < visitas_maximas) {
+            contador_vis++;
+            console.log(mensaje_bv);
+        }
+        else {
+            mensaje_bv = "Adiós, buenas tardes";
+            console.log(mensaje_bv);
+        }
+    }
+}
--- M06/Practica2_Sintaxi_Typescript/src/ts/index.ts
+++ M06/Practica2_Sintaxi_Typescript/src/ts/index.ts
@@ -1,2 +1,39 @@
-var pepe:number=1;
-console.log(pepe);
+
+
+const visitas_maximas: number = 3;
+let nombre_pagina: string;
+let contador_vis: number = 2;
+var mensaje_bv: string = "Hola, bienvenido!";
+
+
+
+do {
+    if(contador_vis < visitas_maximas){
+        contador_vis++;
+        console.log
+    }
+    else if(contador_vis = visitas_maximas){
+        contador_vis++;
+
+    }
+} while(contador_vis < visitas_maximas);
+
+
+if (contador_vis < visitas_maximas) { 
+    contador_vis++; 
+    console.log(mensaje_bv) 
+    if (contador_vis <= visitas_maximas) { 
+        contador_vis++; 
+        console.log(mensaje_bv)
+        if (contador_vis < visitas_maximas) { 
+            contador_vis++; 
+            console.log(mensaje_bv) 
+        }
+        else {
+            mensaje_bv = "Adiós, buenas tardes"; 
+            console.log(mensaje_bv); 
+        }
+    }
+}
+
+
